83bad06cf73d47c56dc9d3e96c199d681045a705
Nature <naturefern@gmail.com>
2024-12-05 23:18:32 -0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,320 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        PanelStart = New Panel()
+        LabelStartButton = New Label()
+        PictureBoxTitle = New PictureBox()
+        Label2 = New Label()
+        PictureBoxStart = New PictureBox()
+        ButtonIngredientA = New Button()
+        ButtonIngredientB = New Button()
+        PictureBoxMain = New PictureBox()
+        ButtonMainMenu = New Button()
+        LabelDialogBox = New Label()
+        LabelDialogEnd = New Label()
+        TimerDialogAnimation = New Timer(components)
+        PanelOptionsMenu = New Panel()
+        Label5 = New Label()
+        Label4 = New Label()
+        ButtonMusicOff = New Button()
+        ButtonMusicOn = New Button()
+        Label1 = New Label()
+        Label3 = New Label()
+        PanelStart.SuspendLayout()
+        CType(PictureBoxTitle, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBoxStart, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBoxMain, ComponentModel.ISupportInitialize).BeginInit()
+        PanelOptionsMenu.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' PanelStart
+        ' 
+        PanelStart.BackColor = Color.Gray
+        PanelStart.Controls.Add(LabelStartButton)
+        PanelStart.Controls.Add(PictureBoxTitle)
+        PanelStart.Controls.Add(Label2)
+        PanelStart.Controls.Add(PictureBoxStart)
+        PanelStart.Location = New Point(302, 27)
+        PanelStart.Name = "PanelStart"
+        PanelStart.Size = New Size(210, 195)
+        PanelStart.TabIndex = 0
+        ' 
+        ' LabelStartButton
+        ' 
+        LabelStartButton.Location = New Point(121, 135)
+        LabelStartButton.Name = "LabelStartButton"
+        LabelStartButton.Size = New Size(38, 15)
+        LabelStartButton.TabIndex = 10
+        LabelStartButton.Text = "START"
+        ' 
+        ' PictureBoxTitle
+        ' 
+        PictureBoxTitle.BackColor = Color.DarkGray
+        PictureBoxTitle.Location = New Point(75, 40)
+        PictureBoxTitle.Name = "PictureBoxTitle"
+        PictureBoxTitle.Size = New Size(60, 20)
+        PictureBoxTitle.SizeMode = PictureBoxSizeMode.AutoSize
+        PictureBoxTitle.TabIndex = 0
+        PictureBoxTitle.TabStop = False
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.BackColor = Color.Transparent
+        Label2.Font = New Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point)
+        Label2.ForeColor = Color.Green
+        Label2.Location = New Point(52, 4)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(110, 25)
+        Label2.TabIndex = 8
+        Label2.Text = "' Start Panel"
+        Label2.Visible = False
+        ' 
+        ' PictureBoxStart
+        ' 
+        PictureBoxStart.BackColor = Color.RosyBrown
+        PictureBoxStart.Location = New Point(24, 24)
+        PictureBoxStart.Name = "PictureBoxStart"
+        PictureBoxStart.Size = New Size(160, 142)
+        PictureBoxStart.SizeMode = PictureBoxSizeMode.CenterImage
+        PictureBoxStart.TabIndex = 2
+        PictureBoxStart.TabStop = False
+        ' 
+        ' ButtonIngredientA
+        ' 
+        ButtonIngredientA.AutoSize = True
+        ButtonIngredientA.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        ButtonIngredientA.BackColor = Color.Tan
+        ButtonIngredientA.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point)
+        ButtonIngredientA.Location = New Point(35, 438)
+        ButtonIngredientA.Name = "ButtonIngredientA"
+        ButtonIngredientA.Size = New Size(134, 42)
+        ButtonIngredientA.TabIndex = 1
+        ButtonIngredientA.Text = "Ingredient"
+        ButtonIngredientA.UseVisualStyleBackColor = False
+        ButtonIngredientA.Visible = False
+        ' 
+        ' ButtonIngredientB
+        ' 
+        ButtonIngredientB.AutoSize = True
+        ButtonIngredientB.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        ButtonIngredientB.BackColor = Color.Tan
+        ButtonIngredientB.Font = New Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point)
+        ButtonIngredientB.Location = New Point(35, 390)
+        ButtonIngredientB.Name = "ButtonIngredientB"
+        ButtonIngredientB.Size = New Size(134, 42)
+        ButtonIngredientB.TabIndex = 2
+        ButtonIngredientB.Text = "Ingredient"
+        ButtonIngredientB.UseVisualStyleBackColor = False
+        ButtonIngredientB.Visible = False
+        ' 
+        ' PictureBoxMain
+        ' 
+        PictureBoxMain.BackColor = Color.RosyBrown
+        PictureBoxMain.Location = New Point(35, 27)
+        PictureBoxMain.Name = "PictureBoxMain"
+        PictureBoxMain.Size = New Size(231, 195)
+        PictureBoxMain.SizeMode = PictureBoxSizeMode.CenterImage
+        PictureBoxMain.TabIndex = 3
+        PictureBoxMain.TabStop = False
+        ' 
+        ' ButtonMainMenu
+        ' 
+        ButtonMainMenu.BackColor = Color.Gray
+        ButtonMainMenu.BackgroundImageLayout = ImageLayout.None
+        ButtonMainMenu.FlatAppearance.BorderSize = 0
+        ButtonMainMenu.FlatStyle = FlatStyle.Flat
+        ButtonMainMenu.Font = New Font("Arial", 24F, FontStyle.Regular, GraphicsUnit.Point)
+        ButtonMainMenu.ForeColor = Color.DarkGray
+        ButtonMainMenu.Location = New Point(203, 54)
+        ButtonMainMenu.Name = "ButtonMainMenu"
+        ButtonMainMenu.Size = New Size(41, 41)
+        ButtonMainMenu.TabIndex = 4
+        ButtonMainMenu.Text = "Menu"
+        ButtonMainMenu.UseVisualStyleBackColor = False
+        ' 
+        ' LabelDialogBox
+        ' 
+        LabelDialogBox.BackColor = SystemColors.ActiveCaption
+        LabelDialogBox.Font = New Font("Arial", 18F, FontStyle.Regular, GraphicsUnit.Point)
+        LabelDialogBox.Location = New Point(52, 162)
+        LabelDialogBox.Name = "LabelDialogBox"
+        LabelDialogBox.Padding = New Padding(2)
+        LabelDialogBox.Size = New Size(128, 41)
+        LabelDialogBox.TabIndex = 5
+        LabelDialogBox.Visible = False
+        ' 
+        ' LabelDialogEnd
+        ' 
+        LabelDialogEnd.BackColor = SystemColors.ActiveCaption
+        LabelDialogEnd.Font = New Font("Wingdings", 26.25F, FontStyle.Regular, GraphicsUnit.Point)
+        LabelDialogEnd.ForeColor = Color.SaddleBrown
+        LabelDialogEnd.Location = New Point(203, 162)
+        LabelDialogEnd.Name = "LabelDialogEnd"
+        LabelDialogEnd.Size = New Size(39, 38)
+        LabelDialogEnd.TabIndex = 6
+        LabelDialogEnd.Text = "û"
+        ' 
+        ' TimerDialogAnimation
+        ' 
+        ' 
+        ' PanelOptionsMenu
+        ' 
+        PanelOptionsMenu.BackColor = Color.Gray
+        PanelOptionsMenu.Controls.Add(Label5)
+        PanelOptionsMenu.Controls.Add(Label4)
+        PanelOptionsMenu.Controls.Add(ButtonMusicOff)
+        PanelOptionsMenu.Controls.Add(ButtonMusicOn)
+        PanelOptionsMenu.Controls.Add(Label1)
+        PanelOptionsMenu.Location = New Point(552, 27)
+        PanelOptionsMenu.Name = "PanelOptionsMenu"
+        PanelOptionsMenu.Size = New Size(265, 287)
+        PanelOptionsMenu.TabIndex = 7
+        PanelOptionsMenu.Visible = False
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point)
+        Label5.ForeColor = SystemColors.ControlDarkDark
+        Label5.Location = New Point(59, 125)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(154, 25)
+        Label5.TabIndex = 11
+        Label5.Text = "Nothing here yet"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.BackColor = Color.Transparent
+        Label4.Font = New Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point)
+        Label4.ForeColor = Color.Green
+        Label4.Location = New Point(103, 1)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(138, 25)
+        Label4.TabIndex = 10
+        Label4.Text = "' Options Panel"
+        Label4.Visible = False
+        ' 
+        ' ButtonMusicOff
+        ' 
+        ButtonMusicOff.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point)
+        ButtonMusicOff.Location = New Point(142, 177)
+        ButtonMusicOff.Name = "ButtonMusicOff"
+        ButtonMusicOff.Size = New Size(55, 37)
+        ButtonMusicOff.TabIndex = 2
+        ButtonMusicOff.Text = "Off"
+        ButtonMusicOff.UseVisualStyleBackColor = True
+        ButtonMusicOff.Visible = False
+        ' 
+        ' ButtonMusicOn
+        ' 
+        ButtonMusicOn.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point)
+        ButtonMusicOn.Location = New Point(79, 177)
+        ButtonMusicOn.Name = "ButtonMusicOn"
+        ButtonMusicOn.Size = New Size(55, 37)
+        ButtonMusicOn.TabIndex = 1
+        ButtonMusicOn.Text = "On"
+        ButtonMusicOn.UseVisualStyleBackColor = True
+        ButtonMusicOn.Visible = False
+        ' 
+        ' Label1
+        ' 
+        Label1.BackColor = Color.Tan
+        Label1.Font = New Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point)
+        Label1.Location = New Point(90, 28)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(88, 40)
+        Label1.TabIndex = 0
+        Label1.Text = "Menu"
+        Label1.Visible = False
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.BackColor = Color.RosyBrown
+        Label3.Font = New Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point)
+        Label3.ForeColor = Color.Green
+        Label3.Location = New Point(61, 36)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(128, 25)
+        Label3.TabIndex = 9
+        Label3.Text = "' Main Picture"
+        Label3.Visible = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        ClientSize = New Size(1264, 681)
+        Controls.Add(ButtonMainMenu)
+        Controls.Add(Label3)
+        Controls.Add(PanelOptionsMenu)
+        Controls.Add(ButtonIngredientB)
+        Controls.Add(ButtonIngredientA)
+        Controls.Add(PanelStart)
+        Controls.Add(LabelDialogEnd)
+        Controls.Add(LabelDialogBox)
+        Controls.Add(PictureBoxMain)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        MaximumSize = New Size(1280, 720)
+        MinimumSize = New Size(1280, 720)
+        Name = "Form1"
+        Text = "Form1"
+        PanelStart.ResumeLayout(False)
+        PanelStart.PerformLayout()
+        CType(PictureBoxTitle, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBoxStart, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBoxMain, ComponentModel.ISupportInitialize).EndInit()
+        PanelOptionsMenu.ResumeLayout(False)
+        PanelOptionsMenu.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PanelStart As Panel
+    Friend WithEvents PictureBoxStart As PictureBox
+    Friend WithEvents PictureBoxTitle As PictureBox
+    Friend WithEvents ButtonIngredientA As Button
+    Friend WithEvents ButtonIngredientB As Button
+    Friend WithEvents PictureBoxMain As PictureBox
+    Friend WithEvents ButtonMainMenu As Button
+    Friend WithEvents LabelDialogBox As Label
+    Friend WithEvents LabelDialogEnd As Label
+    Friend WithEvents TimerDialogAnimation As Timer
+    Friend WithEvents PanelOptionsMenu As Panel
+    Friend WithEvents ButtonMusicOff As Button
+    Friend WithEvents ButtonMusicOn As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents LabelStartButton As Label
+    Friend WithEvents Label5 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,390 @@
+﻿Imports System.IO
+Imports System.Media
+
+Public Class Form1
+    ' Declare arrays to hold paths or indexes
+    Private imageFiles As String()
+    Private currentImageIndex As Integer = 0
+    Private currentDialogIndex As Integer = 0
+    ' Booleans
+    Private IngredientA As Boolean = False
+    Private IngredientB As Boolean = False
+
+    ' Dialog stuff
+    Private Dialogs As List(Of String)
+    Private dialogText As String ' The full dialog text
+    Private currentIndex As Integer ' Current position in the text
+    Private isWriting As Boolean ' Tracks if the writing animation is in progress
+    Private Speednormal As Integer = 50
+    Private Speedfast As Integer = 5
+
+    Public MyUiDirectory As String = Path.Combine(Application.StartupPath, "Soup_Data\ui")
+    Public MyImageDirectory As String = Path.Combine(Application.StartupPath, "Soup_Data\scene")
+    Public MyAudioDirectory As String = Path.Combine(Application.StartupPath, "Soup_Data\audio")
+    Public MyDialogDirectory As String = Path.Combine(Application.StartupPath, "Soup_Data\text")
+    Public ClickPath As String = MyAudioDirectory + "\click.wav"
+    Public MyWindowSize As New Size(1280, 720)
+    Public MyBackgroundColor As Color = Color.FromArgb(184, 132, 88)
+    Public MyForeColor As Color = Color.FromArgb(235, 145, 68)
+    Public MyOtherColor As Color = Color.FromArgb(66, 38, 56) ' Dark Purple
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        InitializeMyPropeties()
+        LoadDialogs() ' Create dialog array from file
+        LoadSceneImages() ' Load Images
+        DisplayImage(imageFiles(1)) ' Display first image
+        ResetIngredients() 'Set all Ingredients to False
+        PanelStart.Visible = True 'Start panel visable
+    End Sub
+
+    ' ### START
+    Private Sub PlayClickSound()
+        Dim player As New SoundPlayer(ClickPath)
+        player.Play()
+    End Sub
+
+    ' ### END 
+    Private Sub InitializeMyPropeties()
+        'Form1 Resize
+        Me.Size = New Size(MyWindowSize)
+
+        ' Start Panel Properties
+        PanelStart.BackColor = MyBackgroundColor
+        PanelStart.Top = 0
+        PanelStart.Left = 0
+        PanelStart.Size = New Size(MyWindowSize)
+        PanelStart.ForeColor = MyForeColor
+
+        ' Start Panel Picture Box Properties
+        PictureBoxStart.BackColor = MyBackgroundColor
+        PictureBoxStart.Top = 0
+        PictureBoxStart.Left = 0
+        PictureBoxStart.Size = MyWindowSize
+        PanelStart.ForeColor = MyForeColor
+
+        ' Start Button Properties
+        LabelStartButton.Size = New Size(Image.FromFile(MyUiDirectory + "\start_button_smaller.png").Size)
+        LabelStartButton.Top = (720 - LabelStartButton.Height) - 75
+        LabelStartButton.Left = (1280 - LabelStartButton.Width) - 75
+        LabelStartButton.Text = ""
+
+        ' Title Picture Box Properties
+        PictureBoxTitle.BackColor = System.Drawing.Color.Transparent
+        PictureBoxTitle.Size = New Size(Image.FromFile(MyUiDirectory + "\title.png").Size)
+        PictureBoxTitle.Top = 0
+        PictureBoxTitle.Left = 640 - (PictureBoxTitle.Width / 2)
+
+        ' Main Picture Box Properties
+        PictureBoxMain.BackColor = MyBackgroundColor
+        PictureBoxMain.Top = -30
+        PictureBoxMain.Left = 0
+        PictureBoxMain.Size = New Size(1280, 720)
+
+        ' Dialog Box
+        LabelDialogBox.Height = 35 ' Height * Set this before end sybol height 
+        LabelDialogBox.Width = 650 ' Width
+        LabelDialogBox.Top = 640 ' Top
+        LabelDialogBox.Left = 640 - (LabelDialogBox.Width / 2)
+        LabelDialogBox.BackColor = MyOtherColor
+        LabelDialogBox.ForeColor = MyForeColor
+        LabelDialogBox.Text = ""
+
+        ' Dialog Box End Symbol
+        LabelDialogEnd.Left = LabelDialogBox.Right - 40
+        LabelDialogEnd.Top = LabelDialogBox.Top
+        LabelDialogEnd.ForeColor = MyForeColor
+        LabelDialogEnd.BackColor = MyOtherColor
+        LabelDialogEnd.ForeColor = MyForeColor
+        LabelDialogEnd.Height = LabelDialogBox.Height
+
+        ' Options Menu Button
+        ButtonMainMenu.Top = 20
+        ButtonMainMenu.Left = 1130
+        ButtonMainMenu.ForeColor = MyForeColor
+        ButtonMainMenu.AutoSize = True
+
+        ' Options Panel 
+        PanelOptionsMenu.Left = (ButtonMainMenu.Left - PanelOptionsMenu.Width) - 10
+        PanelOptionsMenu.Top = ButtonMainMenu.Top
+        PanelOptionsMenu.ForeColor = MyForeColor
+
+        ' Ingredient Buttons
+        ButtonIngredientA.Top = 430 ' A Top
+        ButtonIngredientB.Top = 430 ' B Top
+        ButtonIngredientA.ForeColor = MyForeColor
+        ButtonIngredientA.BackColor = MyOtherColor
+
+        ButtonIngredientA.Left = 410 ' A Left
+        ButtonIngredientB.Left = 790 ' B Left
+        ButtonIngredientB.ForeColor = MyForeColor
+        ButtonIngredientB.BackColor = MyOtherColor
+        ButtonIngredientA.Visible = False
+        ButtonIngredientB.Visible = False
+
+
+
+
+
+
+    End Sub
+
+    ' Method to start animated writing
+    Private Sub StartDialogAnimation(text As String)
+        If isWriting Then Return ' Prevent overlapping animations
+        LabelDialogEnd.Visible = False ' Make dialog X hidden when writing
+        dialogText = text ' Set the full text
+        currentIndex = 0 ' Reset the index
+        LabelDialogBox.Text = "" ' Clear the label
+        isWriting = True
+        TimerDialogAnimation.Start() ' Start the Timer
+        TimerDialogAnimation.Interval = Speednormal ' Reset speed to normal
+    End Sub
+
+    ' Timer tick event for animating the writing
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles TimerDialogAnimation.Tick
+        If dialogText IsNot Nothing Then
+            If currentIndex < dialogText.Length Then
+                ' Append the next character to the label
+                LabelDialogBox.Text &= dialogText(currentIndex)
+                currentIndex += 1
+            Else
+                ' Stop the animation when all text is displayed
+                TimerDialogAnimation.Stop()
+                isWriting = False
+                LabelDialogEnd.Visible = True ' Make X appear for dialog box
+            End If
+        End If
+    End Sub
+
+
+    Private Sub LoadSceneImages()
+        ' Scene Image Array creation
+        ' Check if image directory exists
+        If Directory.Exists(MyImageDirectory) Then
+            ' Load all PNG files in the folder
+            imageFiles = Directory.GetFiles(MyImageDirectory, "*.png").
+                              OrderBy(Function(file) file).
+                              ToArray()
+        Else
+            MessageBox.Show("image folder not found: " & MyImageDirectory)
+        End If
+
+        ' Add image to start panel picture box
+        PictureBoxStart.Image = Image.FromFile(imageFiles(0))
+
+        ' Add image to start title image
+        PictureBoxTitle.Image = Image.FromFile(MyUiDirectory + "\title.png")
+        PictureBoxTitle.BackColor = System.Drawing.Color.Transparent
+
+        ' Start Button texture
+        LabelStartButton.BackgroundImage = Image.FromFile(MyUiDirectory + "\start_button_smaller.png")
+    End Sub
+
+    ' Method to display an image in PictureBox1
+    Private Sub DisplayImage(imagePath As String)
+        If IO.File.Exists(imagePath) Then
+            PictureBoxMain.Image = Image.FromFile(imagePath)
+        Else
+            MessageBox.Show("Error loading image.")
+        End If
+    End Sub
+
+    ' Button to toggle options menu visibility
+    Private Sub OptionsButton_Click(sender As Object, e As EventArgs) Handles ButtonMainMenu.Click
+
+        PanelOptionsMenu.Visible = Not PanelOptionsMenu.Visible
+        Update()
+    End Sub
+
+
+    'Click Event Handler
+    Private Sub ClickEvent()
+
+        If isWriting Then ' If the dialog box is still writing, speed it up, and skip rest of method.
+            TimerDialogAnimation.Interval = Speedfast
+            Return
+        End If
+
+        AdvanceScene()
+
+    End Sub
+
+    Private Sub AdvanceScene()
+        ' BEFORE
+        ' Get file name before scene advancement to see if anything should happen 
+        Dim fileNameBefore As String = IO.Path.GetFileName(imageFiles(currentImageIndex))
+
+        ' If Ingredients are false but the Ingredient buttons are still visable, Return to wait for user to click buttons.
+        If ButtonIngredientA.Visible = True And IngredientA = False And IngredientB = False Then
+            Return
+        End If
+
+        ' Scene 8
+        If fileNameBefore = "i8.png" Then
+            ResetIngredients()
+        End If
+
+        ' Scene 6
+        If fileNameBefore = "i6.png" Then
+            currentImageIndex += 1 ' +1 to index so we skip i7
+        End If
+
+        ' Scene 5
+        If fileNameBefore = "i5.png" And IngredientA = True Then
+            currentImageIndex += 0 ' placeholder. Not changing index since desired image is next anyways. 
+        End If
+        If fileNameBefore = "i5.png" And IngredientB = True Then
+            currentImageIndex += 1 ' +1 to index so we skip i6 
+        End If
+
+        ' ADVANCE
+        ' Check if there are images in the array
+        If imageFiles IsNot Nothing AndAlso imageFiles.Length > 0 Then
+            ' Increment the index to display the next image
+            currentImageIndex = (currentImageIndex + 1) Mod imageFiles.Length
+            DisplayImage(imageFiles(currentImageIndex))
+        End If
+
+        StartDialogAnimation(GetDialog(currentImageIndex)) ' Start dialog animation 
+
+        ' AFTER
+        'Get file name after scene advancement to see if anything special needs to happen
+        Dim fileNameAfter As String = IO.Path.GetFileName(imageFiles(currentImageIndex))
+
+        'if new-scene #, do stuff
+        If fileNameAfter = "i3.png" Then
+            ButtonIngredientA.Visible = True
+            ButtonIngredientB.Visible = True
+        End If
+
+        If fileNameAfter = "i4.png" Then
+            ButtonIngredientA.Visible = False
+            ButtonIngredientB.Visible = False
+        End If
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBoxMain.Click
+        If isWriting Then
+            TimerDialogAnimation.Interval = Speedfast
+            Return
+        End If
+
+        If ButtonIngredientA.Visible Then Return
+
+        ClickEvent()
+    End Sub
+
+    Private Sub LabelDialog_Click(sender As Object, e As EventArgs) Handles LabelDialogBox.Click
+        If isWriting Then
+            TimerDialogAnimation.Interval = Speedfast
+            Return
+        End If
+
+        If ButtonIngredientA.Visible Then Return
+
+        ClickEvent()
+    End Sub
+
+    Private Sub LabelDialogEnd_Click(sender As Object, e As EventArgs) Handles LabelDialogEnd.Click
+        If isWriting Then
+            TimerDialogAnimation.Interval = Speedfast
+            Return
+        End If
+
+        If ButtonIngredientA.Visible Then Return
+
+        ClickEvent()
+    End Sub
+
+
+    Private Sub ButtonIngredientA_Click(sender As Object, e As EventArgs) Handles ButtonIngredientA.Click
+        If isWriting = True Then
+            ClickEvent()
+            Return
+        Else
+            IngredientA = True
+
+            ClickEvent()
+        End If
+        PlayClickSound()
+    End Sub
+
+    Private Sub ButtonIngredientB_Click(sender As Object, e As EventArgs) Handles ButtonIngredientB.Click
+        If isWriting = True Then
+            ClickEvent()
+            Return
+        Else
+            IngredientB = True
+
+            ClickEvent()
+        End If
+        PlayClickSound()
+    End Sub
+
+    ' Start Button Logic 
+
+    Private Sub LabelStartButton_Click(sender As Object, e As EventArgs) Handles LabelStartButton.Click
+
+        '+1 to image index since the first one was used on start panel
+        currentImageIndex += 1
+
+        'Make dialog box visable
+        LabelDialogBox.Visible = True
+
+        'Hide start panel
+        PanelStart.Visible = False
+
+        StartDialogAnimation(GetDialog(currentImageIndex)) 'Start dialog animation
+        PlayClickSound()
+    End Sub
+
+    Private Sub ResetIngredients()
+        IngredientA = False
+        IngredientB = False
+    End Sub
+
+    ' Ensure proper disposal of resources
+    Protected Overrides Sub OnFormClosing(e As FormClosingEventArgs)
+        MyBase.OnFormClosing(e)
+        'StopBackgroundMusic()
+    End Sub
+
+
+
+    Public Sub New()
+        InitializeComponent()
+
+    End Sub
+
+    Private Sub LoadDialogs()
+        Dim dialogpath As String = MyDialogDirectory + "\dialogs.txt"
+
+        If File.Exists(dialogpath) Then
+            ' Read all the dialog content
+            Dim dialogContent As String = File.ReadAllText(dialogpath)
+
+            ' Split the content into sections using '###' as the delimiter
+            Dialogs = dialogContent.Split(New String() {"###"}, StringSplitOptions.RemoveEmptyEntries).
+                                         Select(Function(s) s.Trim()).ToList()
+        Else
+            MessageBox.Show("Dialog file not found in the data folder.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    ' Get dialog by index
+    Private Function GetDialog(index As Integer) As String
+        If index >= 0 AndAlso index < Dialogs.Count Then
+            Return Dialogs(index)
+        Else
+            Return "Dialog not found."
+        End If
+    End Function
+
+    ' Total number of dialogs
+    Private Function GetTotalDialogs() As Integer
+        Return Dialogs.Count
+    End Function
+
+
+End Class
